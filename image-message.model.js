08ae83c314a2c9134777674c1ed29b731c563acf
Jose Berardo Cunha <joseberardo@gmail.com>
2017-01-28 19:16:15 +1100

    ES6 scope start

---
+++ image-message.model.js
@@ -0,0 +1,18 @@
+class ImageMessage extends Message {
+  constructor(text = '', created = Date.now(),
+    url = '', thumbnail = '') {
+    super(text, created);
+    this.url = url;
+    this.thumbnail = thumbnail;
+  }
+
+  /**
+   * Method overriden
+   * @returns String
+   */
+  toString() {
+    return `Photo${super.toString()} ` +
+           `- Url: ${this.url} ` +
+           `- Thumbnail: ${this.thumbnail}`;
+  }
+}
