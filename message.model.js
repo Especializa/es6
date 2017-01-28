9bff62ebc6cc1f6309fc686f574d650f5d9cd616
Jose Berardo Cunha <joseberardo@gmail.com>
2017-01-28 19:16:15 +1100

    ES6 scope start

---
+++ message.model.js
@@ -0,0 +1,25 @@
+class Message {
+  constructor(text = '', created = Date.now()) {
+    this.text = text;
+    this.created = created;
+  }
+  get created() {
+    return this._created;
+  }
+  set created(created) {
+    if (typeof created === 'undefined' || isNaN(created)) {
+      throw new Error('Invalid created');
+    }
+    if (Message.hasOwnProperty.call(this, '_created')) {
+      throw new Error('Created already defined');
+    }
+    this._created = created;
+  }
+  toString() {
+    const { created, text } = this;
+    return `Message created at: ${created} - Text: ${text}`;
+  }
+  static newEmptyMessage() {
+    return new Message();
+  }
+}
