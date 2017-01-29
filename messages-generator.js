70f35bc7bdc8aebd4d144a9398a5ec440f9212e0
Jose Berardo Cunha <joseberardo@gmail.com>
2017-01-29 12:34:14 +1100

    fix trailing comas

--- messages-generator.js
+++ messages-generator.js
@@ -3,7 +3,7 @@ for (let x = 0; x < 50; x += 1) {
   console.log(
   `  {
     "text": "Message ${x + 1}",
-    "created": ${Date.now() - parseInt(Math.random() * 20000, 10)},` +
+    "created": ${Date.now() - parseInt(Math.random() * 20000, 10)}` +
   '\n  },');
 }
 console.log(']');
