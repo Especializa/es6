28eaad343de1685a9a4e43c5af91fd01b296ba40
Jose Berardo Cunha <joseberardo@gmail.com>
2017-03-21 18:53:50 +1100

    Types & Types End

--- app/playground.ts
+++ app/playground.ts
@@ -1,2 +1,30 @@
-import { ImageMessage } from './model/image-message.model';
-import { Message } from './model/message.model';
+import { ImageMessage } from './model';
+import { Message } from './model';
+
+namespace questions {
+  export interface Question {
+    title: string;
+    created: number;
+  }
+  export const q: Question = { title: 'My Question', created: Date.now() };
+}
+
+type myUnionType = Message | questions.Question;
+type myIntersecType = Message & questions.Question;
+
+const x: myUnionType = questions.q;
+
+const y: myIntersecType = {title: 'My Title', text: 'My Text', created: 10};
+
+type Size = 'SMALL' | 'MEDIUM' | 'LARGE';
+
+const s1: Size = 'LARGE';
+
+enum EnumSize {
+  SMALL,
+  MEDIUM,
+  LARGE,
+};
+
+const s2: EnumSize = EnumSize.LARGE;
+const s3: number = s2;
